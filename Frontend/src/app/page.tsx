4e43dbc565a273861217f5d7e6a023850df20c3d
SanjayBarman15 <sanjaybarman1179@gmail.com>
2025-09-16 11:15:20 +0530

    Initial commit

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+ return (
+  <>
+  <h1>hello</h1>
+  <Button>click me</Button>
+  <h1>test</h1>
+  </>
+
+ )
+}
